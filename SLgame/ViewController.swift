c67bda0ecfcaca9f30982d82d89910bb303d28b9
Alisha Tsai <aliciatsai919@gmail.com>
2021-04-24 11:04:37 +0800

    Initial Commit

---
+++ SLgame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SLgame
+//
+//  Created by Alisha on 2021/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
